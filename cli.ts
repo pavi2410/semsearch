428124a09a0b7ef8aae81e907d741ac48cb0e6ca
Pavitra Golchha <hello@pavi2410.me>
2024-10-11 00:10:00 +0530

    highlight spans for better view

--- cli.ts
+++ cli.ts
@@ -1,4 +1,4 @@
-import { TfIdf, WordTokenizer } from "natural";
+import { LevenshteinDistanceSearch, TfIdf, WordTokenizer } from "natural";
 import { styleText } from "node:util";
 import docsList from "./docs.json";
 
@@ -27,6 +27,48 @@ const sortedResults = Array.from(results.entries()).sort((a, b) => b[1] - a[1]);
 
 for (const [docIdx, score] of sortedResults.slice(0, 10)) {
   const doc = docsList[docIdx];
-  console.log(`${styleText('bold', doc.title)} (${score.toFixed(2)})
+
+  console.log(`${styleText('bold', addHighlights(doc.title, tokens))} (${score.toFixed(2)})
 ↳ ${styleText('dim', styleText('underline', doc.url))}\n`);
+}
+
+function highlightSpan(text: string, start: number, end: number) {
+  return text.slice(0, start) + styleText('bgYellow', text.slice(start, end)) + text.slice(end);
+}
+
+type Span = [start: number, end: number];
+function addHighlights(text: string, tokens: string[]) {
+  const lowercasedText = text.toLowerCase();
+  
+  const spans: Span[] = [];
+  for (const token of tokens) {
+    const ld = LevenshteinDistanceSearch(token, lowercasedText);
+    if (ld.distance < token.length / 2) {
+      spans.push([ld.offset, ld.offset + ld.substring.length]);
+    }
+  }
+
+  spans.sort((a, b) => (a[0] === b[0]) ? (a[1] - b[1]) : (a[0] - b[0]));
+
+  const mergedSpans: Span[] = [];
+  for (const span of spans) {
+    if (mergedSpans.length === 0) {
+      mergedSpans.push(span);
+    } else {
+      const lastSpan = mergedSpans[mergedSpans.length - 1];
+      if (span[0] <= lastSpan[1]) {
+        lastSpan[1] = Math.max(lastSpan[1], span[1]);
+      } else {
+        mergedSpans.push(span);
+      }
+    }
+  }
+
+  let m = 0;
+  for (const [start, end] of mergedSpans) {
+    text = highlightSpan(text, m + start, m + end);
+    m += 10; // ANSI escape code length offset added on each highlight
+  }
+
+  return text
 }
