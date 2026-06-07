09edb419b25039b9533223393050ab3186a9a781
Pavitra Golchha <hello@pavi2410.com>
2026-06-08 00:13:51 +0530

    Add top-level uv run scripts: crawl, index, search

--- semsearch/__main__.py
+++ semsearch/__main__.py
@@ -1,24 +1,3 @@
-import sys
+from .cli import main
 
-from . import cli, crawler, indexer
-
-
-def main() -> None:
-    if len(sys.argv) < 2:
-        print("Usage: semsearch <query>")
-        print("   or: semsearch index")
-        print("   or: semsearch crawl")
-        sys.exit(1)
-
-    subcommand = sys.argv[1]
-
-    if subcommand == "index":
-        indexer.main()
-    elif subcommand == "crawl":
-        crawler.main()
-    else:
-        cli.main()
-
-
-if __name__ == "__main__":
-    main()
+main()
