6684eaa1dd60059106813de31b28ada5b577b2b8
Pavitra Golchha <hello@pavi2410.com>
2026-06-11 00:53:41 +0530

    chore: add ruff for linting and formatting

--- semsearch/storage/__init__.py
+++ semsearch/storage/__init__.py
@@ -1,4 +1,11 @@
 from .page import save_page, read_page_meta, iter_page_metas, url_hash
 from .content import save_content, read_content
 
-__all__ = ["save_page", "read_page_meta", "iter_page_metas", "save_content", "read_content", "url_hash"]
+__all__ = [
+    "save_page",
+    "read_page_meta",
+    "iter_page_metas",
+    "save_content",
+    "read_content",
+    "url_hash",
+]
